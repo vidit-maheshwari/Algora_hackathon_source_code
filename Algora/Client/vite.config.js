eedf2f547f28a8542f3c039c73954feeac378024
Vidit Maheshwari <22mc3037@rgipt.ac.in>
2025-04-19 01:37:36 +0530

    docker fix

--- Algora/Client/vite.config.js
+++ Algora/Client/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/@fortawesome/fontawesome-free/webfonts',
-          dest: 'assets',
-        },
-      ],
-    }),
   ],
   server: {
     fs: {
